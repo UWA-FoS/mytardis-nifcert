1a02cc406705f9dd5fb7303794d05713c3f498cf
Dean Taylor <dean.taylor@uwa.edu.au>
2017-12-08 10:04:59 +0800

    Add filter settings

--- settings.d/nifcert.py
+++ settings.d/nifcert.py
@@ -1,3 +1,13 @@
 INSTALLED_APPS += (
     'nifcert',
 )
+
+POST_SAVE_FILTERS.append(
+    (
+        "nifcert.filters.metadata_filter.make_filter",
+        [
+            "NifCertMetadata",
+            "http://trudat.cmca.uwa.edu.au/schemas/datafile/open-format/dicom-stats/1.0",
+        ],
+    ),
+)
