ec8c602cf1957d2963ed27584fd1f1161bfd3b81
avrljk <avrljk@some.host>
2017-12-11 17:30:54 +0800

    Remove apps.py import missed in prev commit

--- apps.py
+++ apps.py
@@ -2,7 +2,6 @@
 # # https://docs.djangoproject.com/en/1.11/ref/applications/
 
 from django.apps import AppConfig
-from tardis.tardis_portal.models import Schema
 
 class NifCertConfig(AppConfig):
     name = 'nifcert'
